b584dd9e254f9d6507cd6d05944335ff757b0fa3
Matthias Dittgen <matthias.dittgen@valiton.com>
2014-10-25 20:28:16 +0200

    Data binding

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
   var Nuts3 = euvis.Nuts3;
   var Contracts = euvis.Contracts;
   var FileLoader = euvis.FileLoader;
+  var Map = euvis.Map;
 
   $("#dataTable").tablesorter();
 
@@ -13,8 +14,7 @@
 
       Nuts3.init(function () {
         Contracts.init(data);
-
-
+        Map.addData(Contracts.getAll());
       });
     }
   })
